95141ec190e2349fa30944a734be440bf871b07a
郑梓健 <1181469095@qq.com>
2022-11-15 11:16:32 +0800

    day6 修改和删除地址

--- src/main/java/com/zzj/waimai/controller/AddressBookController.java
+++ src/main/java/com/zzj/waimai/controller/AddressBookController.java
@@ -52,7 +52,21 @@ public class AddressBookController {
         addressBookService.updateById(addressBook);
         return R.success(addressBook);
     }
-
+    /**根摇地址id删除用户地址
+        param id
+        return
+    */
+    @DeleteMapping
+    public R<String>delete(@RequestParam("ids")Long id){
+        if (id ==null){
+            return R.error("请求异常");
+        }
+            LambdaQueryWrapper<AddressBook>queryWrapper =new LambdaQueryWrapper<>();
+            queryWrapper.eq(AddressBook::getId,id).eq(AddressBook::getUserId,BaseContext.getCurrentId());
+            addressBookService.remove(queryWrapper);
+            addressBookService.removeById(id);//感觉直接使用这个removeById.不太严谨...
+            return R.success("删除地址成功");
+    }
     /**
      * 根据id查询地址
      */
@@ -65,7 +79,18 @@ public class AddressBookController {
             return R.error("没有找到该对象");
         }
     }
-
+    /**修改收货地址
+        @param addressBook
+        return
+    */
+    @PutMapping
+    public R<String>update(@RequestBody AddressBook addressBook){
+        if (addressBook == null){
+            return R.error("请求异常");
+        }
+            addressBookService.updateById(addressBook);
+            return R.success("修改成功");
+    }
     /**
      * 查询默认地址
      */
